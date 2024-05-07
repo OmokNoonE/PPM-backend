e9f2b6da0e5e675247d9a2d9666717a8253343f7
JaeWon Lee <jlee38266@gmail.com>
2024-05-07 19:20:39 +0900

    [fix] ResponseMessage에서 @build 어노테이션 제거

--- src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
+++ src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
@@ -13,8 +13,7 @@ public class ResponseMessage {
     private int httpStatus;                 // 상태코드
     private String message;                 // 응답 메세지
     private Map<String, Object> result;     // 응답 데이터
-
-    @Builder
+    
     public ResponseMessage(int httpStatus, String message, Map<String, Object> result) {
         this.httpStatus = httpStatus;
         this.message = message;
