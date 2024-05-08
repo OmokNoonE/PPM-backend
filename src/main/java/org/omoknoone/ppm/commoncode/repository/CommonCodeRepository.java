c727426f154875ae19cd0c7f9aa0f46970229e36
JaewonLee <jlee38266@gmail.com>
2024-05-09 00:26:09 +0900

    #52 [feat] commonCodeRepository에 메소드 추가
    commonCode entity에서 외래키로 참조하고 있는 codeGroupId를 간접 참조 하기 위해서 commonCodeRepository에 메소드 추가

--- src/main/java/org/omoknoone/ppm/commoncode/repository/CommonCodeRepository.java
+++ src/main/java/org/omoknoone/ppm/commoncode/repository/CommonCodeRepository.java
@@ -1,8 +1,14 @@
 package org.omoknoone.ppm.commoncode.repository;
 
 import org.omoknoone.ppm.commoncode.aggregate.CommonCode;
+import org.omoknoone.ppm.commoncode.dto.CommonCodeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommonCodeRepository extends JpaRepository<CommonCode, Long> {
+
+    // 외래 키를 기준으로 CommonCode 검색
+    List<CommonCodeDTO> findByCodeGroupId(Long codeGroupId);
 
 }
