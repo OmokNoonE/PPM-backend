139fdb1ea029dd52d99cb0bccb2922dc406608d8
CJC0512 <ehdchs2006@gmail.com>
2024-05-09 08:45:04 +0900

    #30 [feat] 일정 등록 기능 개발을 위한 Service 생성

---
+++ src/main/java/org/omoknoone/ppm/schedule/service/ScheduleService.java
@@ -0,0 +1,11 @@
+package org.omoknoone.ppm.schedule.service;
+
+import org.omoknoone.ppm.schedule.dto.NewScheduleDTO;
+
+public interface ScheduleService {
+
+    void createSchedule(NewScheduleDTO newScheduleDTO);
+
+
+
+}
