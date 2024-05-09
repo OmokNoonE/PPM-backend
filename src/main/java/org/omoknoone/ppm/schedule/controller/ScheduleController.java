f646c0c4e50d82904e96cdb57d1601664546ac69
CJC0512 <ehdchs2006@gmail.com>
2024-05-09 11:00:21 +0900

    #30 [feat] 일정 등록 기능 개발을 위한 Controller 생성

---
+++ src/main/java/org/omoknoone/ppm/schedule/controller/ScheduleController.java
@@ -0,0 +1,41 @@
+package org.omoknoone.ppm.schedule.controller;
+
+import org.modelmapper.ModelMapper;
+import org.omoknoone.ppm.schedule.dto.ScheduleDTO;
+import org.omoknoone.ppm.schedule.service.ScheduleService;
+import org.omoknoone.ppm.schedule.vo.RequestSchedule;
+import org.omoknoone.ppm.schedule.vo.ResponseSchedule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedules")
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ScheduleController(ScheduleService scheduleService, ModelMapper modelMapper) {
+        this.scheduleService = scheduleService;
+        this.modelMapper = modelMapper;
+    }
+
+    /* 일정 등록 */
+    @PostMapping("/create")
+    public ResponseEntity<ResponseSchedule> createSchedule(@RequestBody RequestSchedule schedule){
+
+        ScheduleDTO scheduleDTO = modelMapper.map(schedule, ScheduleDTO.class);
+
+        scheduleService.createSchedule(scheduleDTO);
+
+        ResponseSchedule responseSchedule = modelMapper.map(scheduleDTO, ResponseSchedule.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseSchedule);
+    }
+}
