69c7f05552b8f8bfe9325859bc69209b4518cbfc
JaewonLee <jlee38266@gmail.com>
2024-05-10 12:22:09 +0900

    #14 #15 [feat] projectMember controller 셋업

--- src/main/java/org/omoknoone/ppm/domain/projectmember/controller/ProjectMemberController.java
+++ src/main/java/org/omoknoone/ppm/domain/projectmember/controller/ProjectMemberController.java
@@ -1,9 +1,13 @@
 package org.omoknoone.ppm.domain.projectmember.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/projectMembers")
 public class ProjectMemberController {
+
+    @GetMapping("/list/{projectId}")
+
 }
