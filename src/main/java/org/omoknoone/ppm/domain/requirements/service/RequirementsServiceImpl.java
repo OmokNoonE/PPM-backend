34521feda3f4559faa8bec9129d8d07389944853
jihyeongeun <38232456+jihyeongeun@users.noreply.github.com>
2024-05-13 15:26:12 +0900

    Update RequirementsServiceImpl.java
    import java.util.List 추가

--- src/main/java/org/omoknoone/ppm/domain/requirements/service/RequirementsServiceImpl.java
+++ src/main/java/org/omoknoone/ppm/domain/requirements/service/RequirementsServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class RequirementsServiceImpl implements RequirementsService {
 	private final ModelMapper modelMapper;
