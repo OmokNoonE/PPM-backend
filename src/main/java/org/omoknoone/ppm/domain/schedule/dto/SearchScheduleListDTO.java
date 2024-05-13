a0bba8efbc7a4fe2c9efbfae2d56b0bf2eba4497
jihyeongeun <38232456+jihyeongeun@users.noreply.github.com>
2024-05-13 11:51:53 +0900

    Update SearchScheduleListDTO.java
    들여쓰기 수정

--- src/main/java/org/omoknoone/ppm/domain/schedule/dto/SearchScheduleListDTO.java
+++ src/main/java/org/omoknoone/ppm/domain/schedule/dto/SearchScheduleListDTO.java
@@ -8,30 +8,30 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-	@NoArgsConstructor
-	@Getter
-	@Setter
-	@ToString
-	public class SearchScheduleListDTO {
-		private Long scheduleId;
-		private String scheduleTitle;
-		private String scheduleContent;
-		private LocalDate scheduleStartDate;
-		private LocalDate scheduleEndDate;
-		private Integer scheduleProgress;
-		private Long scheduleStatus;
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SearchScheduleListDTO {
+	private Long scheduleId;
+	private String scheduleTitle;
+	private String scheduleContent;
+	private LocalDate scheduleStartDate;
+	private LocalDate scheduleEndDate;
+	private Integer scheduleProgress;
+	private Long scheduleStatus;
 
-		@Builder
+	@Builder
 
-		public SearchScheduleListDTO(Long scheduleId, String scheduleTitle, String scheduleContent,
-			LocalDate scheduleStartDate,
-			LocalDate scheduleEndDate, Integer scheduleProgress, Long scheduleStatus) {
-			this.scheduleId = scheduleId;
-			this.scheduleTitle = scheduleTitle;
-			this.scheduleContent = scheduleContent;
-			this.scheduleStartDate = scheduleStartDate;
-			this.scheduleEndDate = scheduleEndDate;
-			this.scheduleProgress = scheduleProgress;
-			this.scheduleStatus = scheduleStatus;
-		}
+	public SearchScheduleListDTO(Long scheduleId, String scheduleTitle, String scheduleContent,
+		LocalDate scheduleStartDate,
+		LocalDate scheduleEndDate, Integer scheduleProgress, Long scheduleStatus) {
+		this.scheduleId = scheduleId;
+		this.scheduleTitle = scheduleTitle;
+		this.scheduleContent = scheduleContent;
+		this.scheduleStartDate = scheduleStartDate;
+		this.scheduleEndDate = scheduleEndDate;
+		this.scheduleProgress = scheduleProgress;
+		this.scheduleStatus = scheduleStatus;
 	}
+}
