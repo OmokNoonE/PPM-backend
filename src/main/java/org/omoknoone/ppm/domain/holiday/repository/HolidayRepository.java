75ff6105b6dc3474e231b664cb384d45b93484d7
jihyeongeun <hyeongeun0924@gmail.com>
2024-05-17 23:06:01 +0900

    [feat] 공수 계산 기능 구현

--- src/main/java/org/omoknoone/ppm/domain/holiday/repository/HolidayRepository.java
+++ src/main/java/org/omoknoone/ppm/domain/holiday/repository/HolidayRepository.java
@@ -1,7 +1,26 @@
 package org.omoknoone.ppm.domain.holiday.repository;
 
+import java.util.List;
+
 import org.omoknoone.ppm.domain.holiday.aggregate.Holiday;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
+
+	@Query("SELECT h FROM Holiday h WHERE " +
+		"(h.holidayYear = :startYear AND h.holidayMonth = :startMonth AND h.holidayDay >= :startDay) " +
+		"OR (h.holidayYear = :endYear AND h.holidayMonth = :endMonth AND h.holidayDay <= :endDay) " +
+		"OR (h.holidayYear > :startYear AND h.holidayYear < :endYear) " +
+		"OR (h.holidayYear = :startYear AND h.holidayMonth > :startMonth) " +
+		"OR (h.holidayYear = :endYear AND h.holidayMonth < :endMonth)")
+	List<Holiday> findHolidaysBetween(
+		@Param("startYear") int startYear,
+		@Param("startMonth") int startMonth,
+		@Param("startDay") int startDay,
+		@Param("endYear") int endYear,
+		@Param("endMonth") int endMonth,
+		@Param("endDay") int endDay
+	);
 }
