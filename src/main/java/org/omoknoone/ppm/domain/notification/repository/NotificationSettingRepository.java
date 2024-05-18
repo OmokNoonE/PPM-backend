026961069362e32d05fc204ee22f15af66c46c72
JaewonLee <jlee38266@gmail.com>
2024-05-18 17:00:31 +0900

    #50 [feat] 알림 수신 옵션 설정 기능을 위한 repository

---
+++ src/main/java/org/omoknoone/ppm/domain/notification/repository/NotificationSettingRepository.java
@@ -0,0 +1,14 @@
+package org.omoknoone.ppm.domain.notification.repository;
+
+import org.omoknoone.ppm.domain.notification.aggregate.NotificationSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
+
+    @Query("SELECT ns" +
+            " FROM NotificationSetting ns" +
+            " WHERE ns.employeeId = :employeeId")
+    NotificationSetting findByEmployeeId(@Param("employeeId") String employeeId);
+}
