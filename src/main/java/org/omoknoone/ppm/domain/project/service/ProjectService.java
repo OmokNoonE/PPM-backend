23c06b1f0d631acd0b039b5a194abdb60d7fec2c
jihyeongeun <hyeongeun0924@gmail.com>
2024-05-20 22:09:08 +0900

    [feature] 프로젝트 일정을 동일한 간격의 일수대로 10개 구간으로 나누는 기능 구현

--- src/main/java/org/omoknoone/ppm/domain/project/service/ProjectService.java
+++ src/main/java/org/omoknoone/ppm/domain/project/service/ProjectService.java
@@ -1,5 +1,9 @@
 package org.omoknoone.ppm.domain.project.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.omoknoone.ppm.domain.project.dto.CreateProjectRequestDTO;
 import org.omoknoone.ppm.domain.project.dto.ModifyProjectHistoryDTO;
 import org.omoknoone.ppm.domain.project.dto.ModifyProjectRequestDTO;
@@ -8,4 +12,6 @@ public interface ProjectService {
     int createProject(CreateProjectRequestDTO createProjectRequestDTO);
 
     int modifyProject(ModifyProjectHistoryDTO modifyProjectRequestDTO);
+
+	List<LocalDate> divideWorkingDaysIntoTen(LocalDateTime startDate, LocalDateTime endDate);
 }
