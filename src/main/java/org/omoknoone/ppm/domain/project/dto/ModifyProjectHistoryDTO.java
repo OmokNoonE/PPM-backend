cffec4ffcbccbbc4450315dffd1fe873b86fbbc0
ms1011 <wkdalstjr94@gmail.com>
2024-05-20 11:32:52 +0900

    #39 [fix] 프로젝트 날짜 자료형 수정, 일정 조회 시 삭제안된 값만 조회하게 수정

--- src/main/java/org/omoknoone/ppm/domain/project/dto/ModifyProjectHistoryDTO.java
+++ src/main/java/org/omoknoone/ppm/domain/project/dto/ModifyProjectHistoryDTO.java
@@ -1,5 +1,6 @@
 package org.omoknoone.ppm.domain.project.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.Builder;
@@ -16,8 +17,8 @@ public class ModifyProjectHistoryDTO {
 	private Integer projectId;
 	private Integer projectMemberId;
 	private String projectTitle;
-	private LocalDateTime projectStartDate;
-	private LocalDateTime projectEndDate;
+	private LocalDate projectStartDate;
+	private LocalDate projectEndDate;
 	private int projectStatus;
 	private boolean projectIsDeleted;
 	private String projectHistoryReason;
@@ -27,9 +28,9 @@ public class ModifyProjectHistoryDTO {
 
 	@Builder
 	public ModifyProjectHistoryDTO(Integer projectId, Integer projectMemberId, String projectTitle,
-		LocalDateTime projectStartDate, LocalDateTime projectEndDate, int projectStatus, boolean projectIsDeleted,
-		String projectHistoryReason, LocalDateTime projectHistoryModifiedDate, Integer projectHistoryProjectId,
-		Integer projectHistoryProjectMemberId) {
+								   LocalDate projectStartDate, LocalDate projectEndDate, int projectStatus, boolean projectIsDeleted,
+								   String projectHistoryReason, LocalDateTime projectHistoryModifiedDate, Integer projectHistoryProjectId,
+								   Integer projectHistoryProjectMemberId) {
 		this.projectId = projectId;
 		this.projectMemberId = projectMemberId;
 		this.projectTitle = projectTitle;
