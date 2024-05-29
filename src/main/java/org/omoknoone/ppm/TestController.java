06aed3ad85c257036bb97226f3ca6182f0d4a79a
orlzlL <80697609+orlzlL@users.noreply.github.com>
2024-05-29 20:02:46 +0900

    Create TestController.java

---
+++ src/main/java/org/omoknoone/ppm/TestController.java
@@ -0,0 +1,13 @@
+package org.omoknoone.ppm.projectDashboard;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "Hello World";
+	}
+}
