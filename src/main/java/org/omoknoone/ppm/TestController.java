69819a03ce37aeea836d038cc4dc74efa08e99f1
orlzlL <80697609+orlzlL@users.noreply.github.com>
2024-05-30 15:54:51 +0900

    Update TestController.java

--- src/main/java/org/omoknoone/ppm/TestController.java
+++ src/main/java/org/omoknoone/ppm/TestController.java
@@ -1,6 +1,7 @@
 package org.omoknoone.ppm.projectDashboard;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
