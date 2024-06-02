20dd0590df08d760547001733dae1fecd253b046
ms1011 <wkdalstjr94@gmail.com>
2024-06-02 15:39:48 +0900

    #170 [fix] 이해관계자 중괄호 에러 해결

--- src/main/java/org/omoknoone/ppm/domain/stakeholders/repository/StakeholdersRepository.java
+++ src/main/java/org/omoknoone/ppm/domain/stakeholders/repository/StakeholdersRepository.java
@@ -2,13 +2,10 @@ package org.omoknoone.ppm.domain.stakeholders.repository;
 
 import org.omoknoone.ppm.domain.stakeholders.aggregate.Stakeholders;
 import org.omoknoone.ppm.domain.stakeholders.dto.StakeholdersDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.omoknoone.ppm.domain.stakeholders.dto.StakeholdersEmployeeInfoDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public interface StakeholdersRepository extends JpaRepository<Stakeholders, Long
 	@Query("SELECT new org.omoknoone.ppm.domain.stakeholders.dto.StakeholdersDTO(s.stakeholdersId, s.stakeholdersType, s.stakeholdersScheduleId, s.stakeholdersProjectMemberId) " +
 		"FROM Stakeholders s WHERE s.stakeholdersProjectMemberId = :stakeholdersProjectMemberId")
 	List<StakeholdersDTO> findByStakeholdersProjectMemberId(@Param("stakeholdersProjectMemberId") Long stakeholdersProjectMemberId);
-}
+
 
     @Query("SELECT new org.omoknoone.ppm.domain.stakeholders.dto.StakeholdersEmployeeInfoDTO("
         + " st.stakeholdersId, st.stakeholdersType, st.stakeholdersScheduleId, "
