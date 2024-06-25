1f80c88ad0ffad61c77b2e4dcfda080d33877fb4
ms1011 <wkdalstjr94@gmail.com>
2024-06-25 14:11:59 +0900

    [fix] httpStatus 자료형 int로 다시 수정

--- src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
+++ src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
@@ -1,7 +1,9 @@
 package org.omoknoone.ppm.common;
 
-import lombok.*;
-import org.springframework.http.HttpStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Map;
 
@@ -11,18 +13,18 @@ import java.util.Map;
 @ToString
 public class ResponseMessage {
 
-    private HttpStatus httpStatus;                 // 상태코드
+    private int httpStatus;                 // 상태코드
     private String message;                 // 응답 메세지
     private Map<String, Object> result;     // 응답 데이터
 
-    public ResponseMessage(HttpStatus httpStatus, String message, Map<String, Object> result) {
+    public ResponseMessage(int httpStatus, String message, Map<String, Object> result) {
         this.httpStatus = httpStatus;
         this.message = message;
         this.result = result;
     }
 
     // 상태와 메시지만을 포함하는 생성자
-    public ResponseMessage(HttpStatus httpStatus, String message) {
+    public ResponseMessage(int httpStatus, String message) {
         this(httpStatus, message, null);
     }
 }
