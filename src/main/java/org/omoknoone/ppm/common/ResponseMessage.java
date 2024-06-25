6837df8bbbeef91474cf0384520ce1016f28f9a1
ms1011 <wkdalstjr94@gmail.com>
2024-06-25 14:04:44 +0900

    [refactor] ResponseMessage 자료형 수정

--- src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
+++ src/main/java/org/omoknoone/ppm/common/ResponseMessage.java
@@ -1,6 +1,7 @@
 package org.omoknoone.ppm.common;
 
 import lombok.*;
+import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
@@ -10,20 +11,18 @@ import java.util.Map;
 @ToString
 public class ResponseMessage {
 
-    private int httpStatus;                 // 상태코드
+    private HttpStatus httpStatus;                 // 상태코드
     private String message;                 // 응답 메세지
     private Map<String, Object> result;     // 응답 데이터
 
-    public ResponseMessage(int httpStatus, String message, Map<String, Object> result) {
+    public ResponseMessage(HttpStatus httpStatus, String message, Map<String, Object> result) {
         this.httpStatus = httpStatus;
         this.message = message;
         this.result = result;
     }
 
     // 상태와 메시지만을 포함하는 생성자
-    public ResponseMessage(int httpStatus, String message) {
-        this.httpStatus = httpStatus;
-        this.message = message;
-        this.result = null;  // 결과 필드를 명시적으로 null로 설정
+    public ResponseMessage(HttpStatus httpStatus, String message) {
+        this(httpStatus, message, null);
     }
 }
